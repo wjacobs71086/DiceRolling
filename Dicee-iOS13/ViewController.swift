03bec64368acb47714507738ec7136afca49d122
Wesley Jacobs <w.jacobs.632@gmail.com>
2020-03-31 21:19:09 -0700

    added button functionality

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -12,12 +12,14 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
         // This is an image literal. it appears as an icon and allows you to select the image without needing the string identifier.
         // dice1.image = #imageLiteral(resourceName: String)
-        
-        
-        
-        
+ 
     }
 
 
+    @IBAction func rollButton(_ sender: UIButton) {
+        var leftDiceValue = 1;
+        dice1.image = [ #imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo") ,#imageLiteral(resourceName: "DiceThree") ,#imageLiteral(resourceName: "DiceFour") ,#imageLiteral(resourceName: "DiceFive") ,#imageLiteral(resourceName: "DiceSix")  ][leftDiceValue]
+        leftDiceValue += 1
+    }
 }
 
