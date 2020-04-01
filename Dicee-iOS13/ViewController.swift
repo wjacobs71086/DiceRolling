16fa31fc1ce2bbe2a32b64a3ba05fa9e379c4f06
Wesley Jacobs <w.jacobs.632@gmail.com>
2020-03-31 20:58:04 -0700

    initial commit, adding button listener next

---
+++ Dicee-iOS13/ViewController.swift
@@ -0,0 +1,23 @@
+// This is how you make a comment. Which is nice.
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var dice1: UIImageView!
+    
+    @IBOutlet weak var dice2: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        // This is an image literal. it appears as an icon and allows you to select the image without needing the string identifier.
+        // dice1.image = #imageLiteral(resourceName: String)
+        
+        
+        
+        
+    }
+
+
+}
+
