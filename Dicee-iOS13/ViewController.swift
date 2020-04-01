97c3d8b45bf06527cd8da1b68e3b1d7baa5d4620
Wesley Jacobs <w.jacobs.632@gmail.com>
2020-04-01 11:28:47 -0700

    button now randomizes the dice

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -6,12 +6,13 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var dice1: UIImageView!
     @IBOutlet weak var dice2: UIImageView!
-
     @IBAction func rollButton(_ sender: UIButton) {
         // var leftDiceValue = 1;
         let arr = [ #imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo") ,#imageLiteral(resourceName: "DiceThree") ,#imageLiteral(resourceName: "DiceFour") ,#imageLiteral(resourceName: "DiceFive") ,#imageLiteral(resourceName: "DiceSix")  ]
-        dice1.image = arr[Int.random(in: 0...5)]
-        dice2.image = arr[Int.random(in: 0...5)]
+        dice1.image = arr.randomElement()
+        dice2.image = arr.randomElement()
+        
+        
         // When the function is complete and you click the button again the variable leftDiceValue gets recreated and again has a value of 1. For this type of action it's better to create the variable outside the scope of the button listener.
     }
 }
