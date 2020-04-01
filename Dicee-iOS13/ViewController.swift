8c09454806641e94d766b0c1bbf939144cfa96e0
Wesley Jacobs <w.jacobs.632@gmail.com>
2020-03-31 21:30:08 -0700

    dice count in opposite direction

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -12,14 +12,19 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
         // This is an image literal. it appears as an icon and allows you to select the image without needing the string identifier.
         // dice1.image = #imageLiteral(resourceName: String)
- 
+        
     }
-
-
+    
+    var leftDiceValue = 1
+    var rightDiceValue = 5
+    
     @IBAction func rollButton(_ sender: UIButton) {
-        var leftDiceValue = 1;
+        // var leftDiceValue = 1;
         dice1.image = [ #imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo") ,#imageLiteral(resourceName: "DiceThree") ,#imageLiteral(resourceName: "DiceFour") ,#imageLiteral(resourceName: "DiceFive") ,#imageLiteral(resourceName: "DiceSix")  ][leftDiceValue]
+        dice2.image = [ #imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo") ,#imageLiteral(resourceName: "DiceThree") ,#imageLiteral(resourceName: "DiceFour") ,#imageLiteral(resourceName: "DiceFive") ,#imageLiteral(resourceName: "DiceSix")  ][rightDiceValue]
         leftDiceValue += 1
+        rightDiceValue -= 1
+        // When the function is complete and you click the button again the variable leftDiceValue gets recreated and again has a value of 1. For this type of action it's better to create the variable outside the scope of the button listener.
     }
 }
 
